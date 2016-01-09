b393b21c25442a3ed3649917bd0d69ecd7b8fd5b
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-01-08 18:59:29 -0800

    setup changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='PyTeX',
     version='0.0.1',
-    description='Compiler from LaTeX to data object',
+    description='LaTeX i/o for Python',
     long_description='',
     url='https://github.com/deniederhut/PyTeX',
     author='Dillon Niederhut',
