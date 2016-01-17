08700781f43d8cbcc67050dd966cdb250d053c61
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-01-17 15:00:49 -0800

    rewrite of find function

--- PyTeX/json.py
+++ PyTeX/json.py
@@ -1,25 +1,31 @@
-import re
+from fuzzywuzzy import fuzz
 from PyTeX import tex
 
 def loads(f):
     return tex.Parser(tex.tokenize(tex.FileIn(f).read())).parse()
 
-def find(term, obj):
-    result = []
-    if type(obj) == str:
-        if re.search(term, obj):
-            result.append(obj)
-    if type(obj) == list:
-        for item in obj:
-            search = find(term, item)
-            if search:
-                result.append({item : search})
-    if type(obj) == dict:
-        for key in obj:
-            search = find(term, key)
-            if search:
-                result.append({key : search})
-            search = find(term, obj[key])
-            if search:
-                result.append({key : search})
-    return result
+class Finder(object):
+
+    def __init__(self, match_strength):
+        self.match_strength = match_strength
+        self.results = []
+
+    def find(self, term, obj):
+        if isinstance(obj, dict):
+            for item in obj.items():
+                if len(item[1]) <= 1:
+                    key_match = fuzz.ratio(term, item[0])
+                    val_match = fuzz.ratio(term, item[1])
+                    if key_match | val_match > self.match_strength:
+                        self.results.append(obj)
+                elif isinstance(item[1][0], dict):
+                    self.find(term, item[1])
+        elif isinstance(obj, list):
+            for item in obj:
+                self.find(term, item)
+        else:
+            raise TypeError("Expected dict or list")
+
+def find(term, obj, match_strength=50):
+    search = Finder(match_strength)
+    return search.find(term, obj).results
