d1c71b2b48fe7ac2eaa2252b8dcf1392311f0174
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-02-09 12:15:06 -0800

    bib test added

---
+++ test/test_bib.py
@@ -0,0 +1,50 @@
+import pytest
+from PyTeX import json, lexer, bib
+
+@pytest.fixture
+def simple():
+    with open('data/simple.bib') as f:
+        return lexer.FileIn(f, bib.SUB_DICT)
+
+def test_FileIn(f=simple):
+    assert f().read() == """@article{test,   author = {Dillon Niederhut},   title={Structured data with PyTeX},   year=2016 }"""
+
+def test_Parser(f=simple):
+    token_list = []
+    for token in f().tokenize(bib.REGEX_LIST):
+        token_list.append(token.name)
+    assert token_list == [
+    'Item',
+    'Start',
+    'Text',
+    'Next',
+    'Text',
+    'Assign',
+    'Start',
+    'Text',
+    'End',
+    'Next',
+    'Text',
+    'Assign',
+    'Start',
+    'Text',
+    'End',
+    'Next',
+    'Text',
+    'Assign',
+    'Number'
+    'End',
+    'EOF'
+    ]
+
+def test_compiler(f=simple):
+    parser = bib.Parser(f().tokenize(bib.REGEX_LIST))
+    assert parser.parse() == [
+        {
+        'article' : {
+            'author' : 'Dillon Niederhut',
+            'title' : 'Structured data with PyTeX',
+            'year' : 2016
+            }
+        }
+    ]
